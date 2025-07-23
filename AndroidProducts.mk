6049e94bf7b06feb0dc5e3070e8c5b0c9ed662a0
shahqufi <shahqufi@gmail.com>
2025-07-23 21:39:11 +0530

    Add initial Android build configuration files

---
+++ AndroidProducts.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_li9.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_li9-eng
