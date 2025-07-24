fdf574f1acd52721b8dd58492e589142e1e31418
shahqufi <shahqufi@gmail.com>
2025-07-24 12:57:49 +0530

    Add lineage_li9.mk for Tecno Li9 device configuration

---
+++ lineage_li9.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from device makefile.
+$(call inherit-product, device/tecno/li9/device.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+BOARD_VENDOR := TECNO
+PRODUCT_NAME := lineage_li9
+PRODUCT_DEVICE := li9
+PRODUCT_MANUFACTURER := TECNO
+PRODUCT_BRAND := TECNO
+PRODUCT_MODEL := TECNO li9
+
+PRODUCT_GMS_CLIENTID_BASE := android-transsion
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DeviceName=li9 \
+    BuildFingerprint=TECNO/LI9-OP/TECNO-LI9:14/UP1A.231005.007/240130V1177:user/release-keys
+
